7a2a083d515c7882898b1bf24eb5cb865c8eaab0
MiraFox <noc@mirafox.ru>
2016-12-08 15:35:25 +0300

    Optimize docker-entrypoint.sh

--- 7.1/scripts/docker-entrypoint.sh
+++ 7.1/scripts/docker-entrypoint.sh
@@ -2,111 +2,79 @@
 
 set -e
 
-PHP_TZ=`echo ${PHP_TIMEZONE} |sed  's|\/|\\\/|g'`
-
 phpini=/usr/local/etc/php/php.ini
 
-if [ ! -z "${PHP_MEMORY_LIMIT}" ]; then
-    sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEMORY_LIMIT}M/g" ${phpini}
-fi
+# php environment
+PHP_ALLOW_URL_FOPEN=${PHP_ALLOW_URL_FOPEN:-On}
+PHP_DISPLAY_ERRORS=${PHP_DISPLAY_ERRORS:-Off}
+PHP_MAX_EXECUTION_TIME=${PHP_MAX_EXECUTION_TIME:-30}
+PHP_MAX_INPUT_TIME=${PHP_MAX_INPUT_TIME:-60}
+PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT:-128}
+PHP_POST_MAX_SIZE=${PHP_POST_MAX_SIZE:-8}
+PHP_SHORT_OPEN_TAG=${PHP_SHORT_OPEN_TAG:-On}
+PHP_TIMEZONE=${PHP_TIMEZONE:-Europe/Moscow}
+PHP_UPLOAD_MAX_FILEZIZE=${PHP_UPLOAD_MAX_FILEZIZE:-2}
 
-if [ ! -z "${PHP_SHORT_OPEN_TAG}" ]; then
-    sed -i "s/short_open_tag = Off/short_open_tag = ${PHP_SHORT_OPEN_TAG}/g" ${phpini}
-else
-    sed -i "s/short_open_tag = Off/short_open_tag = On/g" ${phpini}
-fi
+PHP_TZ=`echo ${PHP_TIMEZONE} |sed  's|\/|\\\/|g'`
 
-if [ ! -z "${PHP_UPLOAD_MAX_FILEZIZE}" ]; then
-    sed -i "s/upload_max_filesize = 2M/upload_max_filesize = ${PHP_UPLOAD_MAX_FILEZIZE}M/g" ${phpini}
-fi
+# addition modules
+PHP_MODULE_MEMCACHED=${PHP_MODULE_MEMCACHED:-Off}
+PHP_MODULE_REDIS=${PHP_MODULE_REDIS:-Off}
+PHP_MODULE_MONGO=${PHP_MODULE_MONGO:-Off}
+PHP_MODULE_GEARMAN=${PHP_MODULE_GEARMAN:-Off}
+PHP_MODULE_IMAP=${PHP_MODULE_IMAP:-Off}
+PHP_MODULE_LDAP=${PHP_MODULE_LDAP:-Off}
+PHP_MODULE_OPCACHE=${PHP_MODULE_OPCACHE:-Off}
 
-if [ ! -z "${PHP_MAX_EXECUTION_TIME}" ]; then
-    sed -i "s/max_execution_time = 30/max_execution_time = ${PHP_MAX_EXECUTION_TIME}/g" ${phpini}
-fi
+# set timezone
+echo ${PHP_TIMEZONE} | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
 
-if [ ! -z "${PHP_MAX_INPUT_TIME}" ]; then
-    sed -i "s/max_input_time = 60/max_input_time = ${PHP_MAX_INPUT_TIME}/g" ${phpini}
-fi
+sed -i \
+    -e "s/memory_limit = 128M/memory_limit = ${PHP_MEMORY_LIMIT}M/g" \
+    -e "s/short_open_tag = Off/short_open_tag = ${PHP_SHORT_OPEN_TAG}/g" \
+    -e "s/upload_max_filesize = 2M/upload_max_filesize = ${PHP_UPLOAD_MAX_FILEZIZE}M/g" \
+    -e "s/max_execution_time = 30/max_execution_time = ${PHP_MAX_EXECUTION_TIME}/g" \
+    -e "s/max_input_time = 60/max_input_time = ${PHP_MAX_INPUT_TIME}/g" \
+    -e "s/display_errors = Off/display_errors = ${PHP_DISPLAY_ERRORS}/g" \
+    -e "s/post_max_size = 8M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" \
+    -e "s/allow_url_fopen = On/allow_url_fopen = ${PHP_ALLOW_URL_FOPEN}/g" \
+    -e "s/;date.timezone =/date.timezone = ${PHP_TZ}/g" \
+    ${phpini}
 
-if [ ! -z "${PHP_DISPLAY_ERRORS}" ]; then
-    sed -i "s/display_errors = Off/display_errors = ${PHP_DISPLAY_ERRORS}/g" ${phpini}
-fi
-
-if [ ! -z "${PHP_POST_MAX_SIZE}" ]; then
-    sed -i "s/post_max_size = 8M/post_max_size = ${PHP_POST_MAX_SIZE}M/g" ${phpini}
-fi
-
-if [ ! -z "${PHP_ALLOW_URL_FOPEN}" ]; then
-    sed -i "s/allow_url_fopen = On/allow_url_fopen = ${PHP_ALLOW_URL_FOPEN}/g" ${phpini}
-fi
-
-if [ ! -z "${PHP_TIMEZONE}" ]; then
-    sed -i "s/;date.timezone =/date.timezone = ${PHP_TZ}/g" ${phpini}
-else
-    sed -i "s/;date.timezone =/date.timezone = Europe\/Moscow/g" ${phpini}
-fi
-
-if [ -z "${PHP_MODULE_MEMCACHED}" ]; then
+if [ ${PHP_MODULE_MEMCACHED} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-memcached.ini
-else
-    if [ ${PHP_MODULE_MEMCACHED} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-memcached.ini
-    fi
 fi
 
-if [ -z "${PHP_MODULE_REDIS}" ]; then
+if [ ${PHP_MODULE_REDIS} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-redis.ini
-else
-    if [ ${PHP_MODULE_REDIS} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-redis.ini
-    fi
 fi
 
-if [ -z "${PHP_MODULE_MONGO}" ]; then
+if [ ${PHP_MODULE_MONGO} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-mongodb.ini
-else
-    if [ ${PHP_MODULE_MONGO} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-mongodb.ini
-    fi
 fi
 
-if [ -z "${PHP_MODULE_GEARMAN}" ]; then
+if [ ${PHP_MODULE_GEARMAN} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-gearman.ini
-else
-    if [ ${PHP_MODULE_GEARMAN} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-gearman.ini
-    fi
 fi
 
-if [ -z "${PHP_MODULE_IMAP}" ]; then
+if [ ${PHP_MODULE_IMAP} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-imap.ini
-else
-    if [ ${PHP_MODULE_IMAP} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-imap.ini
-    fi
 fi
 
-if [ -z "${PHP_MODULE_LDAP}" ]; then
+if [ ${PHP_MODULE_LDAP} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-ldap.ini
-else
-    if [ ${PHP_MODULE_LDAP} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-ldap.ini
-    fi
 fi
 
-if [ -z "${PHP_MODULE_OPCACHE}" ]; then
+if [ ${PHP_MODULE_OPCACHE} == 'Off' ]; then
     rm -f /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
 else
-    if [ ${PHP_MODULE_OPCACHE} == 'Off' ]; then
-	rm -f /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
-    else
-	echo "opcache.memory_consumption=128
-opcache.interned_strings_buffer=8
-opcache.max_accelerated_files=4000
-opcache.revalidate_freq=2
-opcache.fast_shutdown=1
-opcache.enable_cli=1" > /usr/local/etc/php/conf.d/opcache.ini
-    fi
+    echo "opcache.memory_consumption=128" 	 > /usr/local/etc/php/conf.d/opcache.ini
+    echo "opcache.interned_strings_buffer=8" 	>> /usr/local/etc/php/conf.d/opcache.ini
+    echo "opcache.max_accelerated_files=4000" 	>> /usr/local/etc/php/conf.d/opcache.ini
+    echo "opcache.revalidate_freq=2" 		>> /usr/local/etc/php/conf.d/opcache.ini
+    echo "opcache.fast_shutdown=1" 		>> /usr/local/etc/php/conf.d/opcache.ini
+    echo "opcache.enable_cli=1" 		>> /usr/local/etc/php/conf.d/opcache.ini
 fi
 
 /usr/bin/supervisord -n -c /etc/supervisord.conf
